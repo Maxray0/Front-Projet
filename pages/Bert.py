ba2955d10ccb960a544aa5cd699a85b5fdcd07f7
Maxray <maxoupoke2001@gmail.com>
2025-03-04 15:00:59 +0100

    api

--- pages/Bert.py
+++ pages/Bert.py
@@ -2,7 +2,12 @@ import streamlit as st
 import requests
 
 prompt = st.text_input("Type a message...")
+
+data =  {
+    "text" : prompt+"[MASK]",
+    "top_k" : 5
+}
 if st.button("Send"):
-    response = requests.post("https://api-cloud-g3-8a6978b4682d.herokuapp.com/")
+    response = requests.post("https://api-cloud-g3-8a6978b4682d.herokuapp.com/fill_mask", json=data)
     st.write(response.text)
                              
